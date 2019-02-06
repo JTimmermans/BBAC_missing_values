78cb66a949fa98a99e4bcd7fbbe7df3720edcc6c
JTimmermans <15907409+JTimmermans@users.noreply.github.com>
2019-02-06 16:01:36 +0100

    Updated test.py
    Synthethic data as in example

--- test.py
+++ test.py
@@ -7,19 +7,16 @@ import timeit
 list_of_list = [[2.5, 3.0, 3.2, 5.1, 5.3],
                 [2.5, 3.2, 3.0, 5.0, 5.5],
                 [5.2, 5.2, 0, 3.1, 3.2],
-                [5.3, 5.0, 5.0, 3.2, 3.1],
-                [5.0, 5.2, 5.1, 3.3, 3.1],
+                [0, 5.0, 5.0, 3.2, 3.1],
+                [5.0, 5.2, 5.1, 0, 3.1],
                 [7.8, 8.0, 8.0, 5.2, 5.3],
                 [7.5, 7.8, 7.6, 5.1, 5.2]]
-
-
-# Set automatic numpy conversion
 
 # Create array from lists and convert to rpy2-array
 array = np.array(list_of_list)
 Z = numpy_to_r(array)
 
-# Actual tests
+# Use the prediction algorithm
 test = BBAC(Z, n_cltr_r=3, n_cltr_c=2, distance='e')
 test.coclustering()
 test.predict()
