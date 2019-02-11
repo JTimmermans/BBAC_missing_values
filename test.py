8c75d31960c85da0fa528afaf046ece0f7fd1fe9
JTimmermans <15907409+JTimmermans@users.noreply.github.com>
2019-02-11 14:33:56 +0100

    Updated images

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import timeit
 def basic_test():
     # Generate synthethic data
     list_of_list = [[2.5, 3.0, 3.2, 5.1, 5.3],
-                    [2.5, 3.2, 3.0, 5.0, 5.5],
-                    [5.0, 5.2, 5.1, 0, 3.1],
+                    [2.5, 3.2, 3.0, 5.0, 0.0],
+                    [5.0, 5.2, 5.1, 3.2, 3.1],
                     [5.2, 5.2, 0.0, 3.1, 3.2],
                     [0.0, 5.0, 5.0, 3.2, 3.1],
                     [7.8, 8.0, 8.0, 5.2, 5.3],
-                    [7.5, 7.8, 7.6, 5.1, 5.2]]
+                    [7.5, 7.8, 7.6, 5.1, 0.0]]
 
     # Create array from lists and convert to rpy2-array
     array = np.array(list_of_list)
